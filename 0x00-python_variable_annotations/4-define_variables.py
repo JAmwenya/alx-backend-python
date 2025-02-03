a1e1a217b6b7d8c1469ad2da5df5d5b925efcb81
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2025-02-03 14:53:18 +0300

    correcting linters

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -6,4 +6,4 @@ This module contains annotated variables.
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "ALX"
+school: str = "Holberton"
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function to return a tuple with a string and the square of a number.
+A function to return a tuple with a string & the square of a number.
 """
 
 from typing import Union, Tuple
@@ -8,6 +8,6 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple where the first element is k and the second is the square of v.
+    Returns tuple where the first element is k and the second is square of v.
     """
     return (k, float(v**2))
