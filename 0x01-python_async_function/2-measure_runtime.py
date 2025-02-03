e00b662c4dd38fc09a6cdb3b94307eb6ba1b1d43
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2025-02-03 15:08:46 +0300

    fixing linters

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,9 +7,7 @@ import asyncio
 import time
 import importlib
 
-
 wait_n  = importlib.import_module('1-concurrent_coroutines').wait_n
-
 
 def measure_time(n: int, max_delay: int) -> float:
     """
